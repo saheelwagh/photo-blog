7a0940b7d26d0f8c6e007d897adcdd71c27f5f80
Saheel Wagh <saheelwagh@pop-os.localdomain>
2020-05-08 20:46:08 +0530

    updated bio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,10 @@ export default function Footer({ onClose, isVisible }) {
           <section>
             <h2>More about the blog</h2>
             <p>
-              Shanti Wagh is an amateur nature photographer based in Goa, India. She loves spotting birds and promptly identifying them, discovering indigeneous flowers growing naturally around her home and documenting these through her camera. Her other hobbies include tending to her backyard garden and cokking delicious food. 
+              Shanti Wagh is an amateur nature photographer based in Goa, India. She loves spotting birds and promptly identifying them with the help of her friend and guide: Aradhana Rao Rangahachar, discovering indigeneous flowers growing naturally around her home and documenting these through her camera. Her other hobbies include tending to her backyard garden and cokking delicious food. 
+            </p>
+            <p>
+              Website Developed by <a href="https://www.swagh.xyz">S.Wagh</a>
             </p>
           </section>
          
